89eda6d6c2a76d2263e3897ace047d528cd719f0
Juan <jra1724@gmail.com>
2019-03-12 23:55:01 -0700

    last push for deadline

---
+++ javascript/index.js
@@ -0,0 +1,50 @@
+// dropcontent
+function thisFunction() {
+  document.getElementById("bigContent").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.firstbutton')) {
+    var dropdowns = document.getElementsByClassName("bigdrop-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+// smaller twitter windown
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+ 
+
+// this is the sidebar js
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
